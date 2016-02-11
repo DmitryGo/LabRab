ad9bc0550fa2df53f67ce950116e2f9699192cea
fedyn4ik <2310535@qip.ru>
2016-02-11 23:17:37 +0300

    Добавил чат

---
+++ Laba1FD/messages.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Laba1FD
+{
+    partial class messages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbChat = new System.Windows.Forms.ListBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.btBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbChat
+            // 
+            this.lbChat.FormattingEnabled = true;
+            this.lbChat.Location = new System.Drawing.Point(12, 6);
+            this.lbChat.Name = "lbChat";
+            this.lbChat.Size = new System.Drawing.Size(364, 160);
+            this.lbChat.TabIndex = 0;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(12, 172);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(364, 20);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(12, 198);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(364, 43);
+            this.btSend.TabIndex = 2;
+            this.btSend.Text = "Отправить";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // btBack
+            // 
+            this.btBack.Location = new System.Drawing.Point(301, 247);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(75, 23);
+            this.btBack.TabIndex = 7;
+            this.btBack.Text = "Назад";
+            this.btBack.UseVisualStyleBackColor = true;
+            this.btBack.Click += new System.EventHandler(this.btBack_Click);
+            // 
+            // messages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 279);
+            this.Controls.Add(this.btBack);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.lbChat);
+            this.Name = "messages";
+            this.Text = "Чат";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbChat;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.Button btBack;
+    }
+}
---
+++ Laba1FD/messages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1FD
+{
+    public partial class messages : Form
+    {
+        public messages()
+        {
+            InitializeComponent();
+        }
+        //Метод отправки сообщения
+        private void btSend_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Метод для выхода из чата
+        private void btBack_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
