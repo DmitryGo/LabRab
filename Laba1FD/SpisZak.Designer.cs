032c48e589a5c403003247ef81682887a1e94c34
fedyn4ik <2310535@qip.ru>
2016-03-03 15:09:05 +0300

    изменил форму SpisZak

--- Laba1FD/SpisZak.Designer.cs
+++ Laba1FD/SpisZak.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lb1 = new System.Windows.Forms.ListBox();
+            this.lb2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.FormattingEnabled = true;
+            this.lb1.Location = new System.Drawing.Point(7, 8);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(330, 498);
+            this.lb1.TabIndex = 0;
+            // 
+            // lb2
+            // 
+            this.lb2.FormattingEnabled = true;
+            this.lb2.Location = new System.Drawing.Point(353, 8);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(547, 498);
+            this.lb2.TabIndex = 1;
             // 
             // SpisZak
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(910, 514);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
             this.Name = "SpisZak";
             this.Text = "SpisZak";
             this.ResumeLayout(false);
@@ -42,5 +62,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lb1;
+        private System.Windows.Forms.ListBox lb2;
     }
 }
