7aea6d63d03e0c6203bdffcb74dad652b081b7c8
fedyn4ik <2310535@qip.ru>
2016-03-03 15:16:43 +0300

    v1

--- Laba1FD/SpisZak.Designer.cs
+++ Laba1FD/SpisZak.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.lb1 = new System.Windows.Forms.ListBox();
             this.lb2 = new System.Windows.Forms.ListBox();
+            this.bt1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lb1
@@ -45,14 +46,24 @@
             this.lb2.FormattingEnabled = true;
             this.lb2.Location = new System.Drawing.Point(353, 8);
             this.lb2.Name = "lb2";
-            this.lb2.Size = new System.Drawing.Size(547, 498);
+            this.lb2.Size = new System.Drawing.Size(547, 459);
             this.lb2.TabIndex = 1;
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(771, 478);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(128, 27);
+            this.bt1.TabIndex = 2;
+            this.bt1.Text = "Назад";
+            this.bt1.UseVisualStyleBackColor = true;
             // 
             // SpisZak
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(910, 514);
+            this.Controls.Add(this.bt1);
             this.Controls.Add(this.lb2);
             this.Controls.Add(this.lb1);
             this.Name = "SpisZak";
@@ -66,5 +77,6 @@
 
         private System.Windows.Forms.ListBox lb1;
         private System.Windows.Forms.ListBox lb2;
+        private System.Windows.Forms.Button bt1;
     }
 }
